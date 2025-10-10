202250f8958dddcdeef2d1f9cd4fe35fda7a4b84
theun1c <belovdima1122@yandex.ru>
2025-10-10 03:31:49 +0300

    feat: create month calendar

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
-set :port, 4567
+# Настройка MIME типов
+configure do
+  set :public_folder, 'public'
+  mime_type :js, 'application/javascript'
+  mime_type :css, 'text/css'
+end
+
 set :bind, '0.0.0.0'
+set :port, 4567
+
+$calendar_data = {}
 
 get '/' do
-  'Hello, World!'
+  erb :index
 end
+
---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    var element = document.getElementById('calendar');
+    const today = new Date();
+
+    var calendar = new jsCalendar(element); 
+
+    today.setHours(0, 0, 0, 0);
+    console.log(today.toString());
+    calendar.onDateClick(function(event, date) {
+
+        if (event.target.style.backgroundColor == 'rgb(255, 69, 0)') {
+            event.target.style.backgroundColor = "rgb(255, 215, 0)";
+            event.target.style.color = 'black';
+        } else if (event.target.style.backgroundColor == "rgb(255, 215, 0)") {
+            event.target.style.backgroundColor = 'lightgreen';
+            event.target.style.color = 'black';
+        } else if (event.target.style.backgroundColor == 'lightgreen' && date.toString() != today.toString()) {
+            event.target.style.backgroundColor = 'white';
+            event.target.style.color = 'black';
+        } else if (event.target.style.backgroundColor == 'lightgreen' && date.toString() == today.toString()) {
+            event.target.style.backgroundColor = '#52c9ff';
+            event.target.style.color = 'white';
+        } else {
+            event.target.style.backgroundColor = 'rgb(255, 69, 0)';
+            event.target.style.color = 'black';
+        }
+        console.log(date.toString());
+        console.log(today.toString());
+        console.log(today.toString() == date.toString());
+    });
+
+});
+
