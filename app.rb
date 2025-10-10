097dfbd9235ce9412824c4b64ad09f921dc29629
theun1c <belovdima1122@yandex.ru>
2025-10-10 06:32:29 +0300

    bug: try to upload data

--- app.rb
+++ app.rb
@@ -17,3 +17,26 @@ get '/' do
   erb :index
 end
 
+post '/api/calendar' do
+  data = JSON.parse(request.body.read)
+  $calendar_data[data['date']] = data['color']
+end
+
+# ручка для отладки и проверки хеша
+# get '/api/data' do
+#   $calendar_data.each do |date, color|
+#     puts "#{date} - #{color}"
+#   end
+
+#   $calendar_data.to_json
+# end
+
+
+get '/api/data' do
+  content_type :json 
+  if $calendar_data.empty?
+    {}.to_json
+  else
+    $calendar_data.to_json
+  end
+end  
--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,66 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
 
+    color = '';
+    date = '';
 
     var element = document.getElementById('calendar');
     const today = new Date();
 
-    var calendar = new jsCalendar(element); 
-
+    var calendar = new jsCalendar(element, "30/01/2017", {
+        zeroFill: true,
+        backgroundColor: 'red',
+    }); 
+    
     today.setHours(0, 0, 0, 0);
-    console.log(today.toString());
+    // console.log(today.toString());
+
+    const response = await fetch('/api/data');
+    const data = await response.json();
+    // console.log(data);
+
+
+    calendar.refresh();
+
+    
+
     calendar.onDateClick(function(event, date) {
 
         if (event.target.style.backgroundColor == 'rgb(76, 187, 23)') {
             event.target.style.backgroundColor = "rgb(255, 165, 0)";
             event.target.style.color = 'black';
+            saveData(date, 'rgb(255, 165, 0)');
         } else if (event.target.style.backgroundColor == "rgb(255, 165, 0)") {
             event.target.style.backgroundColor = 'rgb(255, 36, 0)';
             event.target.style.color = 'black';
+            saveData(date, 'rgb(255, 36, 0)');
         } else if (event.target.style.backgroundColor == 'rgb(255, 36, 0)' && date.toString() != today.toString()) {
             event.target.style.backgroundColor = 'white';
             event.target.style.color = 'black';
+            saveData(date, 'white');
         } else if (event.target.style.backgroundColor == 'rgb(255, 36, 0)' && date.toString() == today.toString()) {
             event.target.style.backgroundColor = '#52c9ff';
             event.target.style.color = 'white';
+            saveData(date, '#52c9ff');
         } else {
             event.target.style.backgroundColor = 'rgb(76, 187, 23)';
             event.target.style.color = 'black';
+            saveData(date, 'rgb(76, 187, 23)');
         }
-        console.log(date.toString());
-        console.log(today.toString());
-        console.log(today.toString() == date.toString());
+    });
+});
+
+async function saveData(date, color) {
+    await fetch(`/api/calendar`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            date: date,
+            color: color
+        })
     });
 
-});
+    console.log("Data saved");
+}
 
